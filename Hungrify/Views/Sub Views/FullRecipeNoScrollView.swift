19003e5687b57fc1ab6774113f87bc6b954f16e0
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-02-17 22:16:18 +0100

    Now you can create a PDF from the same view in which you are

--- Hungrify/Views/Sub Views/FullRecipeNoScrollView.swift
+++ Hungrify/Views/Sub Views/FullRecipeNoScrollView.swift
@@ -13,69 +13,72 @@ struct FullRecipeNoScroll: View {
     @State var pdfUrl: URL?
     
     var body: some View {
-        VStack {
-            Image(uiImage: UIImage(named: ricettaStore.arrayRicette[indiceRicetta].imageName)!)
-                .resizable()
-                .frame(width: 420, height: 240)
-                .scaledToFill()
-            
-            HStack{
-                Text(ricettaStore.arrayRicette[indiceRicetta].recipeName)
-                    .foregroundColor(.black)
-                    .font(.system(size: 30))
-                    .fontWeight(.semibold)
-                Spacer()
+            VStack {
+                Image(uiImage: UIImage(named: ricettaStore.arrayRicette[indiceRicetta].imageName)!)
+                    .resizable()
+                    .frame(width: 420, height: 240)
+                    .scaledToFill()
                 
-                Image(systemName: "heart.fill")
-                    .foregroundColor(Color(UIColor(named: "RossoLabel")!))
-                    .font(.system(size: 30))
-            }
-            .frame(width: UIScreen.main.bounds.width * 0.9)
-            .padding(.top, 15)
-            
-            VStack (alignment: .leading){
-                Text("Description")
-                    .foregroundColor(Color(UIColor(named:"RossoLabel")!))
-                    .fontWeight(.semibold)
-                    .font(.system(size: 21))
-                    .padding(.top, 3)
-                
-                Text(ricettaStore.arrayRicette[indiceRicetta].description)
-                    .font(.system(size: 21))
-                    .fontWeight(.regular)
-                    .padding(.top,1)
-                
-                Text("Ingredients")
-                    .foregroundColor(Color(UIColor(named:"RossoLabel")!))
-                    .fontWeight(.semibold)
-                    .font(.system(size: 21))
-                    .padding(.top, 3)
-                
-                
-                ForEach(ricettaStore.arrayRicette[indiceRicetta].ingredients, id: \.self) { ingredient in
-                    Text(ingredient.capitalized)
-                        .font(.system(size: 21))
-                        .fontWeight(.regular)
+                HStack{
+                    Text(ricettaStore.arrayRicette[indiceRicetta].recipeName)
+                        .foregroundColor(.black)
+                        .font(.system(size: 30))
+                        .fontWeight(.semibold)
+                    Spacer()
+                    
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(Color(UIColor(named: "RossoLabel")!))
+                        .font(.system(size: 30))
                 }
+                .frame(width: UIScreen.main.bounds.width * 0.9)
+                .padding(.top, 15)
                 
-                Text("Cooking")
-                    .foregroundColor(Color(UIColor(named:"RossoLabel")!))
-                    .fontWeight(.semibold)
-                    .font(.system(size: 21))
-                    .padding(.top, 3)
-                ForEach(ricettaStore.arrayRicette[indiceRicetta].steps, id: \.self) { step in
-                    Text(step)
+                VStack (alignment: .leading){
+                    Text("Description")
+                        .foregroundColor(Color(UIColor(named:"RossoLabel")!))
+                        .fontWeight(.semibold)
+                        .font(.system(size: 21))
+                        .padding(.top, 3)
+                    
+                    Text(ricettaStore.arrayRicette[indiceRicetta].description)
                         .font(.system(size: 21))
                         .fontWeight(.regular)
                         .padding(.top,1)
+                    
+                    Text("Ingredients")
+                        .foregroundColor(Color(UIColor(named:"RossoLabel")!))
+                        .fontWeight(.semibold)
+                        .font(.system(size: 21))
+                        .padding(.top, 3)
+                    
+                    
+                    ForEach(ricettaStore.arrayRicette[indiceRicetta].ingredients, id: \.self) { ingredient in
+                        Text(ingredient.capitalized)
+                            .font(.system(size: 21))
+                            .fontWeight(.regular)
+                    }
+                    
+                    Text("Cooking")
+                        .foregroundColor(Color(UIColor(named:"RossoLabel")!))
+                        .fontWeight(.semibold)
+                        .font(.system(size: 21))
+                        .padding(.top, 3)
+                    ForEach(ricettaStore.arrayRicette[indiceRicetta].steps, id: \.self) { step in
+                        Text(step)
+                            .font(.system(size: 21))
+                            .fontWeight(.regular)
+                            .padding(.top,1)
+                    }
+                    
                 }
-                
+                .frame(width: UIScreen.main.bounds.width * 0.9)
             }
-            .frame(width: UIScreen.main.bounds.width * 0.9)
-        }
         .task {
-            if let url = await render(view: self){
+            let url = await render(view: self)
+            if pdfUrl == nil {
                 pdfUrl = url
+                print("*****************")
+                print(pdfUrl!)
             }
         }
         .toolbar {
@@ -95,11 +98,3 @@ struct FullRecipeNoScroll_Previews: PreviewProvider {
         
     }
 }
-
-//                        ForEach(ricettaStore.arrayRicette, id: \.self) { ricetta in
-//                            Text(ricetta1.recipeName)
-//                            ForEach(ricetta1.ingredients, id: \.self){ ingredient in
-//                                Text(ingredient)
-//
-//                            }
-//                        }
